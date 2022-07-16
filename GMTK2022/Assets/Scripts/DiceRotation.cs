c23a6ad063370827206be7cb0855945984c4ef42
KevW <wei00033@gmail.com>
2022-07-16 12:10:45 -0400

    Imported any fully created the Dice Prefab
    + Dice Z rotation fix

--- GMTK2022/Assets/Scripts/DiceRotation.cs
+++ GMTK2022/Assets/Scripts/DiceRotation.cs
@@ -5,67 +5,93 @@ using UnityEngine;
 public class DiceRotation : MonoBehaviour
 {
 
-    float yaw;
-    float pitch;
+    public float yaw;
+    public float pitch;
+    public float zRot;
 
     float yawChange = 0f;
     float pitchChange = 0f;
+    float zChange;
 
     public float RandomRangeValue;
     private float randomYaw;
     private float randomPitch;
+    private float randomZ;
     // Start is called before the first frame update
     void Start()
     {
         randomYaw = Random.Range(-RandomRangeValue, RandomRangeValue);
         randomPitch = Random.Range(-RandomRangeValue, RandomRangeValue);
+        randomZ = Random.Range(-RandomRangeValue, RandomRangeValue);
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        if(yawChange < randomYaw)
+        if(RandomRangeValue != 0)
         {
-            yawChange += Time.deltaTime;
-            if(yawChange >= randomYaw)
+            if (yawChange < randomYaw)
             {
-                newRotate();
+                yawChange += Time.deltaTime;
+                if (yawChange >= randomYaw)
+                {
+                    newRotate();
+                }
             }
-        }
-        else
-        {
-            yawChange -= Time.deltaTime;
-            if (yawChange <= randomYaw)
+            else
             {
-                newRotate();
+                yawChange -= Time.deltaTime;
+                if (yawChange <= randomYaw)
+                {
+                    newRotate();
+                }
             }
-        }
 
-        if (pitchChange < randomPitch)
-        {
-            pitchChange += Time.deltaTime;
-            if (pitchChange >= randomPitch)
+            if (pitchChange < randomPitch)
             {
-                newRotate();
+                pitchChange += Time.deltaTime;
+                if (pitchChange >= randomPitch)
+                {
+                    newRotate();
+                }
             }
-        }
-        else
-        {
-            pitchChange -= Time.deltaTime;
-            if (pitchChange <= randomPitch)
+            else
             {
-                newRotate();
+                pitchChange -= Time.deltaTime;
+                if (pitchChange <= randomPitch)
+                {
+                    newRotate();
+                }
             }
+
+            if (zChange < randomPitch)
+            {
+                zChange += Time.deltaTime;
+                if (zChange >= randomZ)
+                {
+                    newRotate();
+                }
+            }
+            else
+            {
+                zChange -= Time.deltaTime;
+                if (zChange <= randomZ)
+                {
+                    newRotate();
+                }
+            }
+            yaw += yawChange;
+            pitch += pitchChange;
+            zRot += zChange;
+            var desiredRotation = Quaternion.Euler(pitch, yaw, zRot);
+            transform.rotation = Quaternion.Lerp(transform.rotation, desiredRotation, Time.deltaTime * 20f);
         }
-        yaw += yawChange;
-        pitch += pitchChange;
-        var desiredRotation = Quaternion.Euler(pitch, yaw, 0f);
-        transform.rotation = Quaternion.Lerp(transform.rotation, desiredRotation, Time.deltaTime * 20f);
     }
 
     void newRotate()
     {
-        randomYaw = Random.Range(-RandomRangeValue, RandomRangeValue);
+       randomYaw = Random.Range(-RandomRangeValue, RandomRangeValue);
         randomPitch = Random.Range(-RandomRangeValue, RandomRangeValue);
+        randomZ = Random.Range(-RandomRangeValue, RandomRangeValue);
     }
 }
